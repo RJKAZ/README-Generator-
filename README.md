cb2ad482ef43650f32be559ae88fe857318c5d1b
Richard Kazanowski <rjkaz@comcast.net>
2020-02-12 17:21:43 -0500

    final update

--- README.md
+++ README.md
@@ -1,23 +1,47 @@
 
-#rjkaz
 
-## My Info
-Github Photo ![https://avatars1.githubusercontent.com/u/55252070?v=4]
-Github Email: [undefined](mailto:undefined)
-Git Hub Username: rjkaz
 
-### My Project
-Title of the project: shit
-Description of the project: shit
-Anything Needed for install?: shit
-How will this project be used?: shit
+![profile-photo](https://avatars1.githubusercontent.com/u/55252070?v=4)
 
-#### Those pesky and annoying legal tid bits
-Which legal lisence does this project have: Apache License 2.0,GNU General Public License v3.0,MIT License,other
-Is anyone else offered credit due for this project: shit
+### **My Info**
+- **Github Email:** &nbsp; [rjkaz@comcast.net](mailto:rjkaz@comcast.net)
+<br/>
+- **Git Hub Username:**  &nbsp; rjkaz
 
-##### How will this project be Tested?
-Very Good Question: shit
+<hr>
+<br/>
 
+### **My Project**
+- **Title of the project:** &nbsp; Distort Into Me
+<br/>
+- **Description of the project:** &nbsp; A javascript powered slideshow that slides thru sequential illustrations set to a specific song.
+<br/>
+- **Anything Needed for install?:** &nbsp; Just a HTML5 compatible web browser (otherwise the music might not play)
+<br/>
+- **How will this project be used?:**  &nbsp; Its designed as consumable content to be watched. 
+<br/>
+- **How will this project be tested?:** &nbsp; by running it and playing it to the end
+
+<hr>
+<br/>
+
+### **Matters of Legality**
+- **Which legal lisence did this project use?:** &nbsp; other
+<br/>
+- **Is anyone else offered credit due for this project?:** &nbsp; The coding and illustration work are of my own creation, but the music track "Disort into Me" was produced by the musician Lain Volta Trzaska for his musical project "She" (also known as "shemusic"). Given the limited and educational scope of this project, my use of the song would fall under Fair Use. 
+
+<hr>
+<br/>
+
+### **What does the future hold for this project?**
+- Where this is a set story....the next step for the concept would be to require user end input to change the outcome of the story. Kind of like a "Choose your own Adventure" book. 
+
+<hr>
+<br/>
+
+<img src="https://img.shields.io/badge/powered%20by-NODE-green" alt="made with node">
+<img src="https://img.shields.io/badge/-100%25%20Javascript-yellow" alt="100% Javascript">
+<img src="https://img.shields.io/badge/-Rutgers%20Coding%20Bootcamp-red" alt="Coding Bootcamp">
+<img src="https://img.shields.io/badge/supercalifragilisticexpialidocious-I%20am%20having%20too%20much%20fun%20with%20these%20badges-blue" alt="don't ask>
 
   
--- index.js
+++ index.js
@@ -1,54 +1,125 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const axios = require('axios');
 
+const generateMarkdown = profileData => {
+  return `
+
+
+![profile-photo](${profileData.avatar_url})
+
+### **My Info**
+- **Github Email:** &nbsp; [${profileData.email}](mailto:${profileData.email})
+<br/>
+- **Git Hub Username:**  &nbsp; ${profileData.username}
+
+<hr>
+<br/>
+
+### **My Project**
+- **Title of the project:** &nbsp; ${profileData.title}
+<br/>
+- **Description of the project:** &nbsp; ${profileData.description}
+<br/>
+- **Anything Needed for install?:** &nbsp; ${profileData.install}
+<br/>
+- **How will this project be used?:**  &nbsp; ${profileData.usage}
+<br/>
+- **How will this project be tested?:** &nbsp; ${profileData.test}
+
+<hr>
+<br/>
+
+### **Matters of Legality**
+- **Which legal lisence did this project use?:** &nbsp; ${profileData.license}
+<br/>
+- **Is anyone else offered credit due for this project?:** &nbsp; ${profileData.contribution}
+
+<hr>
+<br/>
+
+### **What does the future hold for this project?**
+- ${profileData.future}
+
+<hr>
+<br/>
+
+<img src="https://img.shields.io/badge/powered%20by-NODE-green" alt="made with node">
+<img src="https://img.shields.io/badge/-100%25%20Javascript-yellow" alt="100% Javascript">
+<img src="https://img.shields.io/badge/-Rutgers%20Coding%20Bootcamp-red" alt="Coding Bootcamp">
+<img src="https://img.shields.io/badge/supercalifragilisticexpialidocious-I%20am%20having%20too%20much%20fun%20with%20these%20badges-blue" alt="don't ask>
+
+  `
+}
 
 inquirer.prompt([
   {
     type: 'input',
     message: 'What is your Github username?',
     name: 'username',
+    validate: githubInput =>
+    (githubInput ? true : false)
   },
   {
     type: 'input',
-    message: 'What is your Github email address?',
-    name: 'email',
+    message: 'What is your Github Email Address?',
+    name:'email',
   },
   {
     type: 'input',
-    message: 'What is the title of your roject?',
+    message: 'What is the title of your project?',
     name: 'title',
   },
   {
     type: 'input',
-    message: 'Please describe your project.',
+    message: 'In your own words, please describe your project.',
     name: 'description',
   },
   {
     type: 'input',
-    message: 'Is anything required for Installation?',
+    message: 'What is needed for the installation of the project?',
     name: 'install',
   },
   {
     type: 'input',
-    message: 'Which license did you use?.',
+    message: 'How is the project used?',
+    name: 'usage',
+  },
+  {
+    type: 'checkbox',
+    message: 'Which lisence did you use?',
     name: 'license',
-  }
+    choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'other']
+  },
+  {
+    type: 'input',
+    message: 'Did anyone else contribute to the project?',
+    name: 'contribution',
+  },
+  {
+    type: 'input',
+    message: 'How will you test the project?',
+    name: 'test',
+  },
+  {
+    type: 'input',
+    message: 'What is the next step for this project?',
+    name: 'future',
+  },
 
+])
+.then(inquirerResponse => {
+  const queryUrl = `https://api.github.com/users/${inquirerResponse.username}`;
+  axios.get(queryUrl)
+  .then(({ data: { avatar_url } }) => {
+      const profileData = { ...inquirerResponse, avatar_url};
+      const finishedMarkdown = generateMarkdown(profileData);
 
-
-
-
-]).then(responseObj => {
-  console.log(responseObj);
-  const finishedMarkdown = generateMarkdown(responseObj);
-
-  fs.writeFile('./readme.md', finishedMarkdown, err => {
-    if (err) {
-      return console.log(err);
-    }
-    console.log('success!')
-  })
+      fs.writeFile("./readme.md", finishedMarkdown, err => {
+          if (err) {
+              return console.log(err);
+          }
+          console.log("Success!");
+      });
+  });
 });
-
-
-
